6ab3f8c7738a33a8ece61367c60ab3eb8ce90c23
Cloema <marion.ficher@cri-paris.org>
2019-09-23 10:18:24 +0200

    For the log file in the main And the first Class object which is Calcul_salin()

--- calsal.py
+++ calsal.py
@@ -1,22 +1,38 @@
+# -*- coding: utf-8 -*-
+# calsal.py
+# author: CRI students
+# date : september 2019
+# version : V1.0
+
+
 # Operation de calcul de la salinité
 # Etat : formule brute copier ici, il faut l'adapter au code
 
-A =	[0.0080, -0.1692, 25.3851, 14.0941, -7.0261, 2.7081]
-B = [0.0005, -0.0056, -0.0066, -0.0375, 0.0636, -0.0144]
-C = [0.6766097, 0.0200564, 0.000110426, -6.9698E-07, 1.0031E-09]
-K = 0.0162
+'''
+#Classe Calcul_salin for fun
+'''
+class Calcul_salin(object):
+    def __init__(self):
+        self.salin_test = []
+        self.A = [0.0080, -0.1692, 25.3851, 14.0941, -7.0261, 2.7081]
+        self.B = [0.0005, -0.0056, -0.0066, -0.0375, 0.0636, -0.0144]
+        self.C = [0.6766097, 0.0200564, 0.000110426, -6.9698E-07, 1.0031E-09]
+        self.K = 0.0162
 
-def salinity_calculator(temperature, conductivity):
-    R_p = 1. # pour les mesures à faible profondeur
-    r_t = sum(
-        [C[i] * temperature**i for i in range(5)]
-    )
-    R_t = conductivity / (42.914 * r_t)
-
-    return (
-        sum([A[i] * R_t**(int(i)/2.) for i in range(6)])
-        + (
-            ((temperature - 15)/(1 + K * (temperature - 15)))
-            * (sum([B[i] * R_t**(int(i)/2.) for i in range(6)]))
+    def salinity_calculator(self,temperature, conductivity):
+        R_p = 1. # pour les mesures à faible profondeur
+        r_t = sum(
+            [self.C[i] * temperature**i for i in range(5)]
         )
-    )
+        R_t = conductivity / (42.914 * r_t)
+
+        res =  (
+            sum([self.A[i] * R_t**(int(i)/2.) for i in range(6)])
+            + (
+                ((temperature - 15)/(1 + self.K * (temperature - 15)))
+                * (sum([self.B[i] * R_t**(int(i)/2.) for i in range(6)]))
+            )
+        )
+        self.salin_test.append(res)
+        print("I AM IN THE FUNCTION : " , res)
+        return res
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("coucou loulou")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# main.py
+#author : CRI students
+#date : september 2019
+#version : V1.0
+
+
+#import for the log
+import logging
+from datetime import datetime
+from calsal import Calcul_salin
+
+# initialization of log setting
+logging.basicConfig(filename='./app.log',level=logging.INFO)
+logging.info("Beginning of the program\n")
+logging.basicConfig(format= '%(asctime)s %(message)s', datefmt='%I:%M:%S %p  %d/%m/%Y')
+
+
+#and the main starts (principal program)
+if __name__ == "__main__":
+    try:
+        j=1
+        test_des_calculs = Calcul_salin()
+        for i in range (10):
+            j += 0.2
+            print ("COUCOU ", i)
+            print(test_des_calculs.salinity_calculator(i,j))
+
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        logging.info(message)
+        #continue
